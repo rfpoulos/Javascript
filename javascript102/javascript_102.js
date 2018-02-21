95f33fbd1a3a06fd560abcfd90021e04abb595b3
Jaehee Kim <jaeheekim@Jaehees-MacBook-Pro.local>
2018-02-20 19:12:17 -0500

    added couple more exercises

--- javascript102/javascript_102.js
+++ javascript102/javascript_102.js
@@ -3,7 +3,8 @@ function printNumbers(startNumber, endNumber) {
 	for (var count=startNumber; count <= endNumber; count++) {
 		console.log(count);
 	}
-}
+};
+printNumbers(1,10);
 
 // Print a Square
 function printSquare(number) {
@@ -12,6 +13,8 @@ function printSquare(number) {
 	}
 }
 
+printSquare(5)
+
 // print a box
 function printBox(num1, num2) {
 	console.log('*'.repeat(num1))
@@ -19,7 +22,9 @@ function printBox(num1, num2) {
 		console.log('*' + ' '.repeat(num1-2) + '*')
     }
 	console.log('*'.repeat(num1))
-}
+};
+
+printBox(6,4);
 
 // Print a Banner
 function printBanner(text) {
@@ -28,7 +33,11 @@ function printBanner(text) {
 	console.log('* '+ text + ' *')
 	console.log('*'.repeat(len+4))
 	}
-}
+};
+
+printBanner("Digital Crafts")
+
+
 
 // Factor a number 
 function factors(x){
@@ -41,9 +50,72 @@ function factors(x){
 		i++;
     }
 	console.log(factorsList)		
+};
+
+factors(25);
+
+
+// Leetspeak
+var character = {
+	a : '4',
+	e : '3',
+	g : '6',
+	l : '1',
+	o : '0',
+	s : '5',
+	t : '7'
+  }
+  
+  function leetspeak(string) {
+	  res = ''
+	  for (i of string){
+		  if (character[i]) {
+			res += (character[i])
+		  }
+		  else{
+			res += i
+		  }
+	  }
+	  return res;
+  }
+  
+  console.log(leetspeak("leet"))
+
+//   // Leetspeak version 2
+// var character = {
+// 	a : '4',
+// 	e : '3',
+// 	g : '6',
+// 	l : '1',
+// 	o : '0',
+// 	s : '5',
+// 	t : '7'
+//   }
+  
+//   function leetspeak(string) {
+// 	  res = ''
+// 	  for (let i = 0; i < string.length; i++) {
+// 		  if (character[string[i]]){
+// 			res += (character[string[i]])
+// 		  }
+// 		  else{
+// 			res += string[i]
+// 		  }
+// 	  }
+// 	  return res;
+//   }
+  
+//   console.log(leetspeak("I like programming and long rest"))
+
+
+//Long-long Vowels
+function longLongVowels(text) {
+	var vowel = text.replace(/oo/g, 'ooooo');
+	vowel = vowel.replace(/ee/g, 'eeeee');
+	return vowel
 }
 
-// Caesar Cipher
-
-var cipher
+console.log(longLongVowels('good'))
+console.log(longLongVowels('Cheese'))
+console.log(longLongVowels('Man'))
 
